2c227e2d578fe361b82b2d1ed31a617c0806e3be
khalidamairi3 <khalidamairi3@gmail.com>
2021-01-09 21:58:12 -0700

    2 words toppings case

--- src/components/textOrder.vue
+++ src/components/textOrder.vue
@@ -1,48 +1,41 @@
 <template>
   <div id="textOrder">
     <h1>
-      Please Enter your order in the following format (Size - Topping, Topping, Topping,...)
+      Please Enter your order in the following format (Size - Topping, Topping,
+      Topping,...)
     </h1>
-     <v-textarea
-        style="width : 40vw"
-          v-model="orderText"
-          outlined
-          name="input-7-4"
-          label="Outlined textarea"
-        ></v-textarea>
-        <v-btn  color="primary" @click="validateInput(orderText)">
-              Validate
-        </v-btn>
-        <p v-if="!valid"> the input is not valid, please follow the format</p>
-        <modal height="auto" name="bill" id="billModal">
-            
-            <div id="items"> 
-                <h3> Pizza bill</h3>
-                <h4> Items </h4>
-                <div class="item" v-for="order in orders" :key="order.orderText">
-                    <p> {{ order.quantity + " " + order.orderText + ": $" + (order.quantity * order.price ).toFixed(2) }}</p>
-                    <br>
-                    
-
-                </div>
-                <p> {{ "Subtotal: $"+ calculateTotal()}}</p> 
-                <p> {{ "GST: $"+ (total*0.05).toFixed(2) }}</p>
-                <p> {{ "Total: $" + (total * 1.05).toFixed(2) }} </p>
-
-
-            </div>
-
-
-
-
-
-
-
-
-        </modal>
-            
-            
-
+    <v-textarea
+      style="width : 40vw"
+      v-model="orderText"
+      outlined
+      name="input-7-4"
+      label="Outlined textarea"
+    ></v-textarea>
+    <v-btn color="primary" @click="validateInput(orderText)">
+      Validate
+    </v-btn>
+    <p v-if="!valid">the input is not valid, please follow the format</p>
+    <modal height="auto" name="bill" id="billModal">
+      <div id="items">
+        <h3>Pizza bill</h3>
+        <h4>Items</h4>
+        <div class="item" v-for="order in orders" :key="order.orderText">
+          <p>
+            {{
+              order.quantity +
+                " " +
+                order.orderText +
+                ": $" +
+                (order.quantity * order.price).toFixed(2)
+            }}
+          </p>
+          <br />
+        </div>
+        <p>{{ "Subtotal: $" + calculateTotal() }}</p>
+        <p>{{ "GST: $" + (total * 0.05).toFixed(2) }}</p>
+        <p>{{ "Total: $" + (total * 1.05).toFixed(2) }}</p>
+      </div>
+    </modal>
   </div>
 </template>
 
@@ -54,7 +47,7 @@ export default {
     return {
       orderText: "",
       valid: true,
-      total:0,
+      total: 0,
       orders: [],
       prices: {
         Small: {
@@ -66,7 +59,7 @@ export default {
           FetaCheese: 2.0,
           Tomatoes: 2.0,
           Olives: 2.0,
-          price: 12.0
+          price: 12.0,
         },
         Medium: {
           Cheese: 0.75,
@@ -77,7 +70,7 @@ export default {
           FetaCheese: 3.0,
           Tomatoes: 3.0,
           Olives: 3.0,
-          price: 14.0
+          price: 14.0,
         },
         Large: {
           Cheese: 1.0,
@@ -88,14 +81,13 @@ export default {
           FetaCheese: 4.0,
           Tomatoes: 4.0,
           Olives: 4.0,
-          price: 16.0
-        }
-      }
+          price: 16.0,
+        },
+      },
     };
   },
 
   methods: {
-    
     //   this function is used to validate every item in the order text as follows
     //   clear the orders array to start a new bill
     //   spliting lines so each item can be processed individually
@@ -103,12 +95,12 @@ export default {
     //   spliting toppings to chech every topping
     //   gitting rid of all possible spaces that migh the user enter
     //   check if item data is valid to be added to the order array
-    //   check if all data is valid to show the bill 
-    
-     
+    //   check if all data is valid to show the bill
+
     validateInput(orderText) {
-        this.orders = [];
+      this.orders = [];
       let prices = this.prices;
+      orderText = orderText.trim();
       let items = orderText.split("\n");
       let valid = true;
       for (let i = 0; i < items.length; i++) {
@@ -125,16 +117,17 @@ export default {
           valid = false;
           break;
         }
-        itemToppings.forEach(topping => {
-          topping = topping
-            .split(" ")
-            .join("")
-            .trim();
+        itemToppings.forEach((topping) => {
+          topping = topping.trim();
+          if (topping.startsWith("Feta")) {
+            topping = topping.split(" ").join("");
+          }
+
           if (prices[itemSize][topping] == undefined && topping != "") {
             valid = false;
           }
         });
-       
+
         if (valid) {
           this.addToOrders(itemSize, itemToppings);
         } else {
@@ -143,8 +136,8 @@ export default {
       }
 
       this.valid = valid;
-      if(valid){
-          this.$modal.show("bill");
+      if (valid) {
+        this.$modal.show("bill");
       }
     },
     // this is to create an order object that has the order text, quantity and price
@@ -158,7 +151,7 @@ export default {
         "Five",
         "Six",
         "Seven",
-        "Eight"
+        "Eight",
       ];
       let itemPrice = 0;
       let quantity = 1;
@@ -166,24 +159,24 @@ export default {
         itemSize + ", " + digits[itemToppings.length] + " Topping Pizza - ";
       itemPrice += this.prices[itemSize]["price"];
       for (let i = 0; i < itemToppings.length; i++) {
-        let topping = itemToppings[i]
-          .split(" ")
-          .join("")
-          .trim();
+        let topping = itemToppings[i].trim();
+        if (topping.startsWith("Feta")) {
+          topping = topping.split(" ").join("");
+        }
         itemPrice += this.prices[itemSize][topping];
         if (i == itemToppings.length - 1 && itemToppings.length != 1) {
-          orderText += " and " + itemToppings[i].trim();
+          orderText += " and " + topping;
         } else if (i == 0) {
-          orderText += itemToppings[i].trim();
+          orderText += topping;
         } else {
-          orderText += ", " + itemToppings[i].trim();
+          orderText += ", " + topping;
         }
       }
 
       let order = {
         orderText: orderText,
         price: itemPrice,
-        quantity: quantity
+        quantity: quantity,
       };
       let index = this.orderExist(order);
       if (index == -1) {
@@ -201,35 +194,29 @@ export default {
       return -1;
     },
     // to calculate the total before adding GST
-    calculateTotal(){
-        let total = 0;
-        this.orders.forEach( order => {
-            total += order.quantity * order.price;
-        });
-        this.total = total.toFixed(2)
-        return this.total;
-        
-    }
-  }
-
+    calculateTotal() {
+      let total = 0;
+      this.orders.forEach((order) => {
+        total += order.quantity * order.price;
+      });
+      this.total = total.toFixed(2);
+      return this.total;
+    },
+  },
 };
 </script>
 
-
 <style lang="scss" scoped>
-
-#textOrder{
-    display: grid;
-    align-items: center;
-    justify-items: center;
-    height: 100%;
-   
+#textOrder {
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  height: 100%;
 }
-#items{
-    display: grid;
-    align-items: center;
-    justify-items: center;
-    padding: 10%;
+#items {
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  padding: 10%;
 }
-
 </style>
